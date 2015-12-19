a6182d56086b55f698a54c9dcdb10ebe1d6106ed
chim <chim.He@gmail.com>
2015-12-19 22:28:20 +0800

    i don't know say what

---
+++ TechSkillList.md
@@ -0,0 +1,30 @@
+# Tech Skill List
+
+## Languages
+*  C
+* c++
+* java
+* MakeFile
+* bash
+* python
+* markdown
+
+## Tools
+* gcc
+* make
+* git
+* vim
+* android studio
+* grandle
+* ant
+
+---
+* startUML
+* Haroopad
+* FreeMind
+
+## Linux
+
+## Android
+
+
---
+++ android/AndroidSubsystem.mm
@@ -0,0 +1,10 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1443339183682" ID="ID_307626204" MODIFIED="1443339194854" TEXT="Android">
+<node CREATED="1443339199672" ID="ID_1936962539" MODIFIED="1443339208357" POSITION="right" TEXT="GUI subsystem">
+<node CREATED="1443339213700" ID="ID_1452037570" MODIFIED="1443339227466" TEXT="input subsystem"/>
+<node CREATED="1443339228147" ID="ID_1247199561" MODIFIED="1443339245848" TEXT="view subsystem"/>
+</node>
+<node CREATED="1443339255786" ID="ID_818543558" MODIFIED="1443339282108" POSITION="left" TEXT="app assets subsystem"/>
+</node>
+</map>
